dadac28e31c0d904b209f762575350bc7eb77551
NormalMaNong <1512889762@qq.com>
2025-11-18 15:06:58 +0800

    完成撤销，恢复，剪切，复制，粘贴，全选操作

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -28,7 +28,11 @@ MainWindow::MainWindow(QWidget *parent)
     author->setText("郭梓轩");
     ui->statusbar->addPermanentWidget(author);
 
-
+    ui->actionCopy->setEnabled(false);
+    ui->actionCut->setEnabled(false);
+    ui->actionPaste->setEnabled(false);
+    ui->actionRedo->setEnabled(false);
+    ui->actionUndo->setEnabled(false);
 }
 
 MainWindow::~MainWindow()
@@ -178,5 +182,56 @@ bool MainWindow::userEditComfired()
         }
     }
     return true;
+}
+
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+    ui->actionPaste->setEnabled(true);
+}
+
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+    ui->actionPaste->setEnabled(true);
+}
+
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+
+void MainWindow::on_textEdit_copyAvailable(bool b)
+{
+    ui->actionCopy->setEnabled(b);
+    ui->actionCut->setEnabled(b);
+}
+
+
+void MainWindow::on_textEdit_redoAvailable(bool b)
+{
+    ui->actionRedo->setEnabled(b);
+}
+
+
+void MainWindow::on_textEdit_undoAvailable(bool b)
+{
+    ui->actionUndo->setEnabled(b);
 }
 
