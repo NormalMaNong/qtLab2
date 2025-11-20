a69da73521b2c3f7661a7acec7ec568ea88a1c74
NormalMaNong <1512889762@qq.com>
2025-11-21 02:26:13 +0800

    实现行号显示

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,11 +19,11 @@ MainWindow::MainWindow(QWidget *parent)
     textChanged = false;
     on_actionNew_triggered();
 
-    statusLabel.setMaximumWidth(150);
+    statusLabel.setMaximumWidth(200);
     statusLabel.setText("length: " + QString::number(0) + "  lines: " + QString::number(1));
     ui->statusbar->addPermanentWidget(&statusLabel);
 
-    statusCursorLabel.setMaximumWidth(150);
+    statusCursorLabel.setMaximumWidth(200);
     statusCursorLabel.setText("Ln: " + QString::number(0) + "  Col: " + QString::number(1));
     ui->statusbar->addPermanentWidget(&statusCursorLabel);
 
@@ -39,9 +39,10 @@ MainWindow::MainWindow(QWidget *parent)
 
     on_actionLineWrap_triggered();
 
-     ui->actionStatus->setChecked(true);
+    ui->actionStatus->setChecked(true);
     ui->actionToolBox->setChecked(true);
-
+    ui->actionShowLine->setChecked(false);
+    on_actionShowLine_triggered(false);
 }
 
 MainWindow::~MainWindow()
@@ -165,6 +166,8 @@ void MainWindow::on_textEdit_textChanged()
         this->setWindowTitle("*" + this->windowTitle());
         textChanged = true;
     }
+    statusLabel.setText("length: " + QString::number(ui->textEdit->toPlainText().length()) +
+                        "  lines: " + QString::number(ui->textEdit->document()->lineCount()));
 }
 
 bool MainWindow::userEditComfired()
@@ -322,5 +325,32 @@ void MainWindow::on_actionExit_triggered()
 {
     if(userEditComfired())
         exit(0);
+}
+
+
+void MainWindow::on_textEdit_cursorPositionChanged()
+{
+    int col = 0;
+    int ln = 0;
+    int flg = -1;
+    int pos = ui->textEdit->textCursor().position();
+    QString text = ui->textEdit->toPlainText();
+    for(int i = 0; i < pos; i++){
+        if(text[i] == '\n'){
+            ln++;
+            flg = i;
+        }
+    }
+    flg++;
+    col = pos - flg;
+    statusCursorLabel.setText("Ln: " + QString::number(ln + 1) +
+                              "  Col: " + QString::number(col + 1));
+
+}
+
+
+void MainWindow::on_actionShowLine_triggered(bool checked)
+{
+    ui->textEdit->hideLineNumberArea(!checked);
 }
 
